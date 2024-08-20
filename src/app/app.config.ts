8b0082e23fa27d08270e9ebee7bb79e71e1144bb
Juan Carlos Triana <juancarlostriana80@gmail.com>
2024-08-20 15:15:15 -0500

    find errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { favoritesReducer } from './store/favorites/favorites.reducer'; // Asegúrate que esta ruta sea la correcta
 
 export const appConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideStore({ favorites: favoritesReducer })  
   ]
 };
